70f4ad276881d70af7cebeb0f2a05fa9b744d199
Patrick Laabs <PLaabs@eitco.de>
2022-02-04 17:21:36 +0100

    puppet runs to exec.cmd

--- main.go
+++ main.go
@@ -6,27 +6,30 @@ import (
 	"os/exec"
 )
 
-// func puppet() {
-// 	puppetCmd := exec.Command("puppet", "apply", "--noop", "--test", "--debug", "manifest/manifest.pp")
-
-// 	puppetIn, _ := puppetCmd.StdinPipe()
-// 	puppetOut, _ := puppetCmd.StdoutPipe()
-// 	puppetCmd.Start()
-// 	puppetIn.Write([]byte())
-// }
-
 func main() {
 
-	grepCmd := exec.Command("grep", "hello")
+	puppetCmd := exec.Command("puppet", "apply", "--noop", "--test", "--debug", "manifest/manifest.pp")
 
-	grepIn, _ := grepCmd.StdinPipe()
-	grepOut, _ := grepCmd.StdoutPipe()
-	grepCmd.Start()
-	grepIn.Write([]byte("hello grep\ngoodbye grep"))
-	grepIn.Close()
-	grepBytes, _ := io.ReadAll(grepOut)
-	grepCmd.Wait()
+	puppetIn, _ := puppetCmd.StdinPipe()
+	puppetOut, _ := puppetCmd.StdoutPipe()
+	puppetCmd.Start()
+	puppetIn.Close()
+	puppetBytes, _ := io.ReadAll(puppetOut)
+	puppetCmd.Wait()
 
-	fmt.Println("> grep hello")
-	fmt.Println(string(grepBytes))
+	fmt.Println("> grep puppet stuff")
+	fmt.Println(string(puppetBytes))
+
+	// grepCmd := exec.Command("grep", "hello")
+
+	// grepIn, _ := grepCmd.StdinPipe()
+	// grepOut, _ := grepCmd.StdoutPipe()
+	// grepCmd.Start()
+	// grepIn.Write([]byte("hello grep\ngoodbye grep"))
+	// grepIn.Close()
+	// grepBytes, _ := io.ReadAll(grepOut)
+	// grepCmd.Wait()
+
+	// fmt.Println("> grep hello")
+	// fmt.Println(string(grepBytes))
 }
