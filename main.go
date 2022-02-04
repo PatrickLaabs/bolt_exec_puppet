3ba42a5ada85b6afdb25a544e57952b0e2795a06
Patrick Laabs <PLaabs@eitco.de>
2022-02-04 17:17:30 +0100

    updating repo

--- main.go
+++ main.go
@@ -6,9 +6,18 @@ import (
 	"os/exec"
 )
 
+// func puppet() {
+// 	puppetCmd := exec.Command("puppet", "apply", "--noop", "--test", "--debug", "manifest/manifest.pp")
+
+// 	puppetIn, _ := puppetCmd.StdinPipe()
+// 	puppetOut, _ := puppetCmd.StdoutPipe()
+// 	puppetCmd.Start()
+// 	puppetIn.Write([]byte())
+// }
+
 func main() {
 
-	grepCmd := exec.Command("grep", "goodbye")
+	grepCmd := exec.Command("grep", "hello")
 
 	grepIn, _ := grepCmd.StdinPipe()
 	grepOut, _ := grepCmd.StdoutPipe()
