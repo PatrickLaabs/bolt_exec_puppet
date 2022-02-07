21012f98cdcb741a5183323463530426a35b805b
Patrick Laabs <PLaabs@eitco.de>
2022-02-07 21:26:54 +0100

    StdinPipe and StdoutPipe added with additional err handle

--- main.go
+++ main.go
@@ -10,20 +10,34 @@ import (
 func main() {
 	cmd := exec.Command("/usr/local/bin/puppet", "agent", "--test", "--noop")
 	// cmd := exec.Command("/bin/sh", "-c", "ls | grep fdfd")
-	stdout, err := cmd.StdoutPipe()
+	// stdout, err := cmd.StdoutPipe()
+	Stdin, err := cmd.StdinPipe()
 	if err != nil {
+		// fmt.Println("> stdoutpipe err")
+		fmt.Println("> stdinpipe err")
+		log.Fatal(err)
+	}
+
+	Stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		fmt.Println("> stdoutpipe err")
 		log.Fatal(err)
 	}
 	if err := cmd.Start(); err != nil {
+		fmt.Println("> start err")
 		log.Fatal(err)
 	}
+
+	Stdin.Close()
+
 	// if err := cmd.Close(); err != nil {
 	// 	log.Fatal(err)
 	// }
 
-	Bytes, _ := io.ReadAll(stdout)
+	Bytes, _ := io.ReadAll(Stdout)
 
 	if err := cmd.Wait(); err != nil {
+		fmt.Println("> wait err")
 		log.Fatal(err)
 	}
 
