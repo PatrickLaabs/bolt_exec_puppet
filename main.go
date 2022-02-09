e1869b2d523162d25ea7428f575fef8959545368
PatrickLaabs <patrick.laabs@me.com>
2022-02-09 22:57:00 +0100

    Adding switch statement for err handling

--- main.go
+++ main.go
@@ -2,15 +2,54 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"os"
 	"os/exec"
+	"strings"
+	"syscall"
 )
 
-func main() {
-	cmd := exec.Command("/bin/sh", "-c", "sudo /usr/local/bin/puppet agent --test --noop")
-	stdoutStderr, err := cmd.CombinedOutput()
+func printCommand(cmd *exec.Cmd) {
+	fmt.Printf("==> Executing: %s\n", strings.Join(cmd.Args, " "))
+}
+
+func printError(err error) {
 	if err != nil {
-		log.Fatal(err)
+		os.Stderr.WriteString(fmt.Sprintf("==> Error: %s\n", err.Error()))
 	}
-	fmt.Printf("%s\n", stdoutStderr)
+}
+
+func printOutput(outs []byte) {
+	if len(outs) > 0 {
+		fmt.Printf("==> Output: %s\n", string(outs))
+	}
+}
+
+func fooOutput(outs []byte) {
+	// if len(outs) > 0 {
+	// 	fmt.Printf("==> fooOutput: %s\n", string(outs))
+	// }
+	switch len(outs) {
+	case 1:
+		fmt.Printf("==> switch case 1: %s\n", string(outs))
+	case 2:
+		fmt.Printf("==> switch case 2: %s\n", string(outs))
+	}
+
+}
+
+func main() {
+	cmd := exec.Command("go", "blurb")
+	var waitStatus syscall.WaitStatus
+	if err := cmd.Run(); err != nil {
+		printError(err)
+		if exitError, ok := err.(*exec.ExitError); ok {
+			waitStatus = exitError.Sys().(syscall.WaitStatus)
+			printOutput([]byte(fmt.Sprintf("%d", waitStatus.ExitStatus())))
+			fooOutput([]byte(fmt.Sprintf("%d", waitStatus.ExitStatus())))
+		}
+	} else {
+		waitStatus = cmd.ProcessState.Sys().(syscall.WaitStatus)
+		printOutput([]byte(fmt.Sprintf("%d", waitStatus.ExitStatus())))
+		fooOutput([]byte(fmt.Sprintf("%d", waitStatus.ExitStatus())))
+	}
 }
