539bd79d8897f39a9664354675b16a0273dec453
PatrickLaabs <patrick.laabs@me.com>
2022-02-18 13:56:45 +0100

    adding some arrays

--- testingDir/main.go
+++ testingDir/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"flag"
 	"fmt"
 	"os"
 	"os/exec"
@@ -10,14 +11,83 @@ import (
 )
 
 func main() {
+	goCmd := flag.NewFlagSet("go", flag.ExitOnError)
+	goName := goCmd.String("version", "version", "version")
+
+	gitCmd := flag.NewFlagSet("git", flag.ExitOnError)
+	gitName := gitCmd.String("version", "blurb", "version")
+
+	helpCmd := flag.NewFlagSet("help", flag.ExitOnError)
+	helpName := helpCmd.String("help", "", "-h")
+
+	buildCmd := flag.NewFlagSet("build", flag.ExitOnError)
+	buildName := buildCmd.String("build", "build", "build")
+
+	if len(os.Args) < 2 {
+		fmt.Println(">> Usage:\n>> ./main go or git")
+		os.Exit(1)
+	}
+
+	var n string
+	var ta string
+	var ch [2]string
+	var cha []string
+	var ccc string
+	flag.Parse()
+	// args := flag.Args()
+	switch os.Args[1] {
+	case "go":
+		goCmd.Parse(os.Args[2:])
+		fmt.Println("  tail:", goCmd.Args())
+		fmt.Println("  > tail:", flag.Args())
+		fmt.Println("subcommand 'go'")
+		tail := goCmd.Args()
+		tailConv := strings.Join(tail, " ")
+		n = *goName
+		ta = tailConv
+	case "git":
+		gitCmd.Parse(os.Args[2:])
+		fmt.Println("  tail:", gitCmd.Args())
+		fmt.Println("  > tail:", flag.Args())
+		fmt.Println("subcommand 'git'")
+		fmt.Println("gitName:", *gitName)
+		tail := gitCmd.Args()
+		tailConv := strings.Join(tail, " ")
+		n = *gitName
+		ta = tailConv
+	case "build":
+		buildCmd.Parse(os.Args[2:])
+		fmt.Println("  tail:", buildCmd.Args())
+		fmt.Println("  > tail:", flag.Args())
+		n = *buildName
+		tail := buildCmd.Args()
+		tailConv := strings.Join(tail, " ")
+		ta = tailConv
+		ch[0] = n
+		ch[1] = ta
+		fmt.Println("output from array:", ch[0]+" "+ch[1])
+		cha = ch[1:2]
+		fmt.Printf("type of cha %T\n", cha)
+		fmt.Println("cha output:", cha)
+		chaConv := strings.Join(cha, " ")
+		fmt.Println("chaConv out:", chaConv)
+		ccc = chaConv
+	case "help":
+		helpCmd.Parse(os.Args[2:])
+		fmt.Println(">> Usage:\n>> ./main go or git")
+		//fmt.Println("helpName:", *helpName)
+		n = *helpName
+		os.Exit(1)
+	}
 	// == Command Executing ==
 	// p := "/usr/local/bin/puppet"
 	//p := "/opt/puppetlabs/puppet/bin/puppet"
 	//pw := "puppet"
 	//pa := "agent"
 	//t := "--test"
-
-	cmd := exec.Command("go", "blurb")
+	g := "go"
+	cmd := exec.Command(g, n, ccc)
+	// cmd := exec.Command(g, cha)
 	//if runtime.GOOS == "windows" {
 	//	fmt.Println("Running on Windows:")
 	//	cmd = exec.Command(pw, pa, t, n)
@@ -84,8 +154,8 @@ func exitHandle(outs []byte) {
 	}()
 
 	// Printing depends on case.
-	// Currently it prints the used case and the expected exit code along with it.
-	// This is only for debuging and tests - can be removed afterwards.
+	// Currently, it prints the used case and the expected exit code along with it.
+	// This is only for debugging and tests - can be removed afterwards.
 	switch n := string(outs); n {
 	case "1":
 		fmt.Printf("==> switch case 1 - exit code 1: %s\n", string(outs))
